08e6fb83c6a215cc150495b3d7adf0b2887c25da
Asche <s.asche@yahoo.fr>
2014-08-10 17:43:41 +0200

    Test

--- ventilos.h
+++ ventilos.h
@@ -6,7 +6,7 @@
 //=========================================================================================================//
 // _______________________________________ VENTILATEURS __________________________________________________
 #define MAX_NB_VENTILO 4
-#define PWM_VENTILATEUR_MIN 85
+#define PWM_VENTILATEUR_MIN 0
 typedef struct Ventilo {
   int pin;
   char *name;
