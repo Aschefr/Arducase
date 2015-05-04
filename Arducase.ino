abec852ad78ce22bd6d0e9a1bd025fe20c1ee5af
Quentin <s.asche@yahoo.fr>
2015-05-04 15:02:50 +0200

    Lecture de débit commenté

--- Arducase.ino
+++ Arducase.ino
@@ -263,16 +263,14 @@ void lcd_temp_draw (void) {
 
 // ________________ Calcul flow ______________________
 //=====================================================//
-volatile int NbTopsFan; //measuring the rising edges of the signal
-int Calc;                               
-int hallsensor = 20;    //The pin location of the sensor
- 
-void rpm ()     //This is the function that the interupt calls 
-{ 
-  NbTopsFan++;  //This function measures the rising and falling edge of the 
-
-//hall effect sensors signal
-} 
+//volatile int NbTopsFan; //measuring the rising edges of the signal
+//int Calc;                               
+//int hallsensor = 20;    //The pin location of the sensor
+// 
+//void rpm ()     //This is the function that the interupt calls 
+//{ 
+//  NbTopsFan++;  //This function measures the rising and falling edge of the 
+//} 
 //=====================================================//
 // ________________ Calcul flow ______________________
 
@@ -321,9 +319,9 @@ void setup (void)
   pinMode(tec_pump, OUTPUT);
 
 //Flow
-  pinMode(hallsensor, INPUT); //initializes digital pin 2 as an input
-  Serial.begin(9600); //This is the setup function where the serial port is 
-  attachInterrupt(3, rpm, RISING); //and the interrupt is attached
+//  pinMode(hallsensor, INPUT); //initializes digital pin 2 as an input
+//  Serial.begin(9600); //This is the setup function where the serial port is 
+//  attachInterrupt(3, rpm, RISING); //and the interrupt is attached
 
   // on initialise les modes et tout
   // l'ordre est important !!!!!
@@ -349,13 +347,13 @@ void loop (void) {
   handle_cpu_mem();
 
 //Flow
-  NbTopsFan = 0;	//Set NbTops to 0 ready for calculations
-  sei();		//Enables interrupts
-  delay (1000);	//Wait 1 second
-  cli();		//Disable interrupts
-  Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate 
-  Serial.print (Calc, DEC); //Prints the number calculated above
-  Serial.print (" L/min\r\n"); //Prints "L/hour" and returns a  new line
+//  NbTopsFan = 0;	//Set NbTops to 0 ready for calculations
+//  sei();		//Enables interrupts
+//  delay (1000);	//Wait 1 second
+//  cli();		//Disable interrupts
+//  Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate 
+//  Serial.print (Calc, DEC); //Prints the number calculated above
+//  Serial.print (" L/min\r\n"); //Prints "L/hour" and returns a  new line
   
 
   //digitalWrite(tec_pump, HIGH); //Allume pompe TEC
